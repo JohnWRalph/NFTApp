f006c8de4a91f927e0edbb782307b203d8e811d8
JohnWRalph <John.Ralph.Git@gmail.com>
2023-03-08 19:34:11 -0600

    added index.js

--- index.js
+++ index.js
@@ -8,29 +8,7 @@ Web3 = require("web3")
 
 
 let address;
-async function connectMetaMask() {
-    // connectMetaMaskButton.disabled = true;
-    const provider = new ethers.providers.Web3Provider(
-        (window).ethereum
-    );
-    const walletAddress = await window.ethereum.request({
-        method: "eth_requestAccounts",
-        params: [
-            {
-                eth_accounts: {}
-            }
-        ]
-    });
 
-
-    const accounts = await provider.send("eth_requestAccounts", []);
-    // console.log(provider.send("eth_requestAccounts", []));
-    const providedAccount = accounts[0];
-    console.log(providedAccount)
-    // ethereumAccount.set(providedAccount);
-    return providedAccount;
-
-}
 
 
 async function fetchLoopringNFts() {
@@ -72,8 +50,7 @@ async function fetchLoopringNFts() {
 
 }
 
-var button = document.getElementById("connectMetaMask");
-button.addEventListener("click", connectMetaMask);
+
 
 var button = document.getElementById("fetchLoopring");
 button.addEventListener("click", fetchLoopringNFts);
